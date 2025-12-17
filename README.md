6b5b9ee3cb671dd773349723b4d5ca7a541297af
haikuquiz <haikuquizofficial@gmail.com>
2025-12-17 15:53:01 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Haiku Quiz
+
+Quiz giornaliero con classifica.
+
+## URL
+- Utenti: https://haikuquiz.it
+- Admin: https://haikuquiz.it/admin
+
+## Admin Access
+Email: haikuquizofficial@gmail.com
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7pNNbwdvRJ1zuRkdqi7nBFsb43-fP49g",
+  authDomain: "haiku-quiz.firebaseapp.com",
+  projectId: "haiku-quiz",
+  storageBucket: "haiku-quiz.firebasestorage.app",
+  messagingSenderId: "22024455002",
+  appId: "1:22024455002:web:1513775bad8c2a12aee963"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
